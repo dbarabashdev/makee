88f96bf5ab9f580773aa3cc088e7c65761312970
dbarabashdev <dbarabashdev@gmail.com>
2020-01-15 23:53:01 +0200

    finally done

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
-const { createGitignoreConfig } = require("./core/make-gitignore")
-const { createPrettierRCConfig } = require("./core/make-prettier")
+const fs = require("fs")
+const { GITIGNORE_CONFIG, PRETTIER_RC_CONFIG } = require("./configs")
 
+const createGitignoreConfig = () => {
+  fs.writeFile(".gitignore", GITIGNORE_CONFIG, err => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("Created file .gitignore")
+  })
+}
 
-createPrettierRCConfig()
+const createPrettierRCConfig = () => {
+  fs.writeFile(".prettierrc", PRETTIER_RC_CONFIG, err => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("Created file .prettierrc")
+  })
+}
+
 createGitignoreConfig()
+createPrettierRCConfig()
